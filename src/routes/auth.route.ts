3d63ce25b2876db25dd79245f933168632e36628
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-03 15:20:04 +0545

    updating the path in auth route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,7 +4,6 @@ import { AuthController } from "../controllers/auth.controller";
 let authController = new AuthController();
 const router = Router();
 
-router.post("/register", authController.register)
-router.post("/login", authController.login)
+
 
 export default router;
