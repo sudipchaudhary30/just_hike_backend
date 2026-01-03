637dc32aa804d5a40083de9bd36828696e47d526
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-03 15:17:28 +0545

    fsing the LoginUserDTI

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -21,7 +21,8 @@ export const CreateUserDTO = UserSchema.pick(
 export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
 
 export const LoginUserDTO = z.object({
-    
+    email: z.email(),
+    password: z.string().min(6)
 });
 
 export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
