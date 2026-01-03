30a75c17276ae8ca6840a6c904c55520c581a872
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-03 15:20:47 +0545

    fixing the post route in route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,6 +4,7 @@ import { AuthController } from "../controllers/auth.controller";
 let authController = new AuthController();
 const router = Router();
 
-
+router.post("/register", authController.register)
+router.post("/login", authController.login)
 
 export default router;
