4ef1ac14241fc7cffd0cc5d829d4fea8398992e1
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-03 15:22:52 +0545

    completed the Sprint 2

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { connectDatabase } from './database/mongodb';
 import { PORT } from './config';
 import authRoutes from "./routes/auth.route";
 
-
+const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
