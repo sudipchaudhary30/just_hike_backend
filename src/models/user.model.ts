692595a7bee1c317c263416add5d997d226484b9
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-03 15:19:18 +0545

    fixing the userscheme ins usermodel

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,7 +2,13 @@ import mongoose, { Document, Schema } from "mongoose";
 import { UserType } from "../types/user.type";
 const UserSchema: Schema = new Schema<UserType>(
     {
-      
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        name: { type: String, required: true },
+        role: {
+            type: String,
+            enum: ['admin', 'guide', 'user'],
+            default: 'user',
         }
     },
     {
