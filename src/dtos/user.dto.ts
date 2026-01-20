5bfa371dacd0277400cd90005a4b65c175b27865
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-20 16:58:02 +0545

    updated the LoginuserDTO

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -20,7 +20,9 @@ export const CreateUserDTO = UserSchema.pick(
 )
 export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
 
-
+export const LoginUserDTO = z.object({
+    email: z.email(),
+    password: z.string().min(6)
 });
 
 export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
