6e4045dd7c60d9f57782017f9343d309ad95f1ff
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-20 16:49:05 +0545

    updated the profile and phonenumber interface

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,7 +1,8 @@
 export interface UserType {
     email: string;
     password: string;
-   
+    name: string;
+    phoneNumber?: string;
     role?: 'admin' | 'guide' | 'user';
     profilePicture?: string;
     createdAt?: Date;
