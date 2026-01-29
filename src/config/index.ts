7858cdb201e110043c71e987910653e4ae545584
Sudip Chaudhary <chsudip30@gmail.com>
2026-01-30 01:10:18 +0545

    feat: export JWT_SECRET from config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT: number= process.env.PORT ? parseInt(process.env.PORT) : 5050;
-export const MONGODB_URI: string = process.env.MONGODB_URI || 'mongodb://localhost:27017/35a_backend';
-export const JWT_SECRET : string = process.env.JWT_SECRET || 'default_secret';
+export const MONGODB_URI: string = process.env.MONGODB_URI || 'mongodb://localhost:27017/just_hike';
+export const JWT_SECRET : string = process.env.JWT_SECRET || 'just_hike_secret';
 
 
 
